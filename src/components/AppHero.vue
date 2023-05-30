9e1e3872f30f43cf01e73debd8b63b1ab7d30986
Fef97 <federicakr97@gmail.com>
2023-05-30 16:48:57 +0200

    esercizio + bonus completati

---
+++ src/components/AppHero.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+    name: "AppHero"
+}
+
+</script>
+
+<template>
+    <div class="jumbotron">
+        <img src="../assets/img/Jumbotron.png" alt="">
+    </div>
+</template>
+
+<style scoped>
+.jumbotron {
+    width: 800px;
+    height: 400px;
+    margin: 30px auto;
+    background-color: blue;
+}
+
+img {
+    width: 800px;
+}
+</style>
